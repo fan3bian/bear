22bf72001447b30cadc2b0334c75e374c48307c2
zhangshuyi1 <zhangshuyi1@jd.com>
2018-10-13 21:17:15 +0800

    “a

--- name.sh
+++ name.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 ##    User inputs his first name and last name.  Program shows his full name.
-echo "please input your first name"
-read firstName
-echo "please input your last name"
-read lastName
-echo "your full name is ${firstName} ${lastName}"
+# echo "please input your first name"
+# read firstName
+# echo "please input your last name"
+# read lastName
+# echo "your full name is ${firstName} ${lastName}"
+ 
+ 
+ 
+# second way
+read -p "please input your first name : " firstName
+read -p "please input your last name : " lastName
+echo -e "\nyour full name is ${firstName} ${lastName}"
+
