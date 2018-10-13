7d995fe7011c4cbe9b82bd41a780fe2c333cc837
zhangshuyi1 <zhangshuyi1@jd.com>
2018-10-13 19:54:42 +0800

    first

---
+++ name.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+##    User inputs his first name and last name.  Program shows his full name.
+echo "please input your first name"
+read firstName
+echo "please input your last name"
+read lastName
+echo "your full name is ${firstName} ${lastName}"
